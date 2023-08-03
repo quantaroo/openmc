5d595b35a013b128baab6f44b1e006e7d1c394b4
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-08-03 06:19:44 -0700

    Update settings.cpp

--- src/settings.cpp
+++ src/settings.cpp
@@ -454,10 +454,11 @@ void read_settings_xml(pugi::xml_node root)
       if (ends_with(path, ".mcpl") || ends_with(path, ".mcpl.gz")) {
         auto sites = mcpl_source_sites(path);
         model::external_sources.push_back(make_unique<FileSource>(sites));
-      }else if (ends_with(path, ".mcpl") || ends_with(path, ".mcpl.gz")||ends_with(path, ".h5") || ends_with(path, ".h5.gz")){
-        source_file = true;
       }else {
         model::external_sources.push_back(make_unique<FileSource>(path));
+      }
+      if (ends_with(path, ".mcpl") || ends_with(path, ".mcpl.gz")||ends_with(path, ".h5") || ends_with(path, ".h5.gz")){
+        source_file = true;
       }
     } else if (check_for_node(node, "library")) {
       // Get shared library path and parameters
