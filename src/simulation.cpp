f6bb93a37706b6ef6fbd37375cdb6a95c74773e4
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-08-04 12:06:45 -0700

    Update simulation.cpp

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -583,7 +583,7 @@ void initialize_history(Particle& p, int64_t index_source)
     if(settings::survival_normalization && settings::survival_biasing && settings::weight_cutoff!=NULL&& settings::weight_survive!=NULL&&p.wgt()!=NULL){
       settings::weight_cutoff = settings::weight_cutoff_fixed * p.wgt();
       settings::weight_survive = settings::weight_survive_fixed * p.wgt();
-      std::cout<<"Weight Cutoff: " << settings::weight_cutoff << " Weight Survive: " << settings::weight_survive << " Current Weight: " << p.wgt() << std::endl;
+      std::cout<<"Weight Cutoff: " << settings::weight_cutoff << " Weight Survive: " << settings::weight_survive << " Current Weight: " << p.wgt() << "\n";
     }
   }
   
